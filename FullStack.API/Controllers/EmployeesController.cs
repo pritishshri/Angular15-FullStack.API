e0a05bd73f419ab9d453d664f4ab7711a75afc1e
Pritish Shrikhande <pritish@imocha.io>
2023-04-04 03:30:13 +0530

    Add project files.

---
+++ FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,93 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDBContext _fullStackDBContext;
+
+        public EmployeesController(FullStackDBContext fullStackDBContext)
+        {
+            _fullStackDBContext = fullStackDBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStackDBContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+
+            await _fullStackDBContext.Employees.AddAsync(employee);
+            await _fullStackDBContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployeeById([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDBContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee employeeDetails)
+        {
+            var employee = await _fullStackDBContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = employeeDetails.Name;
+            employee.Email=employeeDetails.Email;
+            employee.Phone = employeeDetails.Phone;
+            employee.Salary= employeeDetails.Salary;
+            employee.Department= employeeDetails.Department;
+
+            await _fullStackDBContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDBContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStackDBContext.Employees.Remove(employee);
+            await _fullStackDBContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+    }
+}
